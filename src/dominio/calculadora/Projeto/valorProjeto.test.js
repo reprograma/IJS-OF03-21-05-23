56c8d021d8366c92eb79a3ab49a0a31d1fac00a3
DoriansFer <doohfernandes@yahoo.com>
2023-05-21 19:14:41 -0300

    Exercicios oficina teste com jest- Doria

--- src/dominio/calculadora/Projeto/valorProjeto.test.js
+++ src/dominio/calculadora/Projeto/valorProjeto.test.js
@@ -1,4 +1,6 @@
 const { calcularValorTotalProjeto} =  require ('./valorProjeto');
+const { calcularHorasDeProjeto } = require('./horasPorProjeto');
+const { calcularValorBaseProjeto } = require ('./valorProjeto');
 
 const pacote = require ('./pacote')
 
@@ -26,4 +28,30 @@ describe ('valor do projeto', ()=>{
         expect(result).toEqual(5544)
 
     })
+
+
+})
+describe ('valor base do projeto', ()=>{
+    beforeEach (() => {
+        pacote.calcularPacote.mockReturnValue('pacote_basico');
+    })
+    test('Retornar valor base do Projeto, para questão de visualização',()=>{
+        const funcionalidades = [
+            'setup',
+            'responsividade',
+            'construcao_1_pagina',
+            'construcao_1_pagina',
+            'construcao_1_pagina',
+            'formulario',
+            'ssr' 
+        ]
+
+        const valorHora = 70;
+        
+        const totalDeHorasPorProjeto = calcularHorasDeProjeto(funcionalidades);
+
+        const result = calcularValorBaseProjeto (totalDeHorasPorProjeto, valorHora);
+
+        expect (result).toEqual(5040)
+    } )
 })
