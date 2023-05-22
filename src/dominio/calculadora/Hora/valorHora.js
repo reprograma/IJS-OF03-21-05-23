23cf9a2a5c7c8b666ac75b8b04f6e3494074b2b3
Mariana Faustino <marifaustino@gmail.com>
2023-05-22 16:28:53 -0300

    Condição sobre renda e lançamento de Erro com test

--- src/dominio/calculadora/Hora/valorHora.js
+++ src/dominio/calculadora/Hora/valorHora.js
@@ -4,10 +4,14 @@ const {
 } = require('../constantes/constantes')
 
 const calcularValorPorHora = (rendaMensal) => {
+  if(rendaMensal <= 0){
+    throw new Error("A renda mensal precisa ser maior que zero.")
+  }else{
   const horasTrabalhadasPorMes = (HORAS_POR_DIA * DIAS_UTEIS_NO_MES);
   const valorPorHora = rendaMensal / horasTrabalhadasPorMes;
 
   return Math.ceil(valorPorHora);
+  }
 };
 
 exports.calcularValorPorHora = calcularValorPorHora;
--- src/entregasDesafio/mariana-faustino/valorHora.test.js
+++ src/entregasDesafio/mariana-faustino/valorHora.test.js
@@ -1,13 +1,15 @@
 const { calcularValorPorHora } = require('../../dominio/calculadora/Hora/valorHora')
 
-test('retornar o valor arredondado correto dado o valor recebido por mês', () =>{
-    expect(calcularValorPorHora(5000)).toBe(29)
-})
-
 describe('Funções para testar valor hora trabalhada', () =>{
+    test('retornar o valor arredondado correto dado o valor recebido por mês', () =>{
+    expect(calcularValorPorHora(5000)).toBe(29) //usando toBe
+})
     test("retornar o valor arredondado correto dado o valor recebido por mês", () =>{
         const valorPorMes = 5000
         const result = calcularValorPorHora(valorPorMes)
-        expect(result).toEqual(29)
+        expect(result).toEqual(29) //usando toEqual
+    })
+    test("lançar erro caso a renda mensal seja menor ou igual a zero.", ()=>{
+        expect(()=> calcularValorPorHora(0)).toThrow(Error)
     })
 })
