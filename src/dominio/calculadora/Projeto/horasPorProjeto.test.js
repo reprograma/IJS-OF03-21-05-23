876cc4521ada50c135ddb0e3ff4ed85c59b48efa
ericasouzaa <ericamilenaa@gmail.com>
2023-05-22 08:28:52 -0300

    Testes modificados e finalizados para seleção da Reprograma

--- src/dominio/calculadora/Projeto/horasPorProjeto.test.js
+++ src/dominio/calculadora/Projeto/horasPorProjeto.test.js
@@ -1,18 +1,40 @@
 const { calcularHorasDeProjeto } = require('./horasPorProjeto');
 
-describe('Cálculo de horas totais para finalizar um projeto', () =>{
-    test('Dado um array com as funcionalidades desejadas no projeto, retornar a soma das horas das funcionalidades', () =>{
-        const funcionalidades = [
-            'setup',
-            'responsividade',
-            'construcao_1_pagina',
-            'construcao_1_pagina',
-            'construcao_1_pagina',
-            'formulario',
-            'ssr'
-        ]
-        const result = calcularHorasDeProjeto(funcionalidades);
+describe('Cálculo de horas totais para finalizar um projeto', () => {
+  test('Dado um array com as funcionalidades desejadas no projeto, retornar a soma das horas das funcionalidades', () => {
+    const funcionalidades = [
+      'setup',
+      'responsividade',
+      'construcao_1_pagina',
+      'integracao_mailchimp',
+      'otimizacao_seo',
+      'integracao_api_propria',
+      'formulario',
+      'ssr'
+    ];
+    const result = calcularHorasDeProjeto(funcionalidades);
 
-        expect(result).toEqual(78)
-    })
-})
+    expect(result).toEqual(78);
+  });
+
+  test('Verificar se a função retorna 0 quando o array de funcionalidades estiver vazio', () => {
+    const funcionalidades = [];
+    const result = calcularHorasDeProjeto(funcionalidades);
+
+    expect(result).toEqual(0);
+  });
+
+  test('Verificar se a função retorna 0 quando o array de funcionalidades for nulo', () => {
+    const funcionalidades = null;
+    const result = calcularHorasDeProjeto(funcionalidades);
+
+    expect(result).toEqual(0);
+  });
+
+  test('Verificar se a função retorna 0 quando o array de funcionalidades for undefined', () => {
+    const funcionalidades = undefined;
+    const result = calcularHorasDeProjeto(funcionalidades);
+
+    expect(result).toEqual(0);
+  });
+});
--- src/dominio/calculadora/Projeto/valorProjeto.test.js
+++ src/dominio/calculadora/Projeto/valorProjeto.test.js
@@ -1,28 +1,77 @@
 const { calcularValorTotalProjeto } = require('./valorProjeto');
-const pacote = require('./pacote')
+const pacote = require('./pacote');
 
-jest.mock('./pacote.js')
-
+const {
+  DIAS_UTEIS_NO_MES,
+  HORAS_POR_FUNCIONALIDADE,
+  TAXAS_CONTRATUAIS_POR_PACOTE
+} = require('../constantes/constantes'); 
+jest.mock('./pacote.js');
 
 describe('valor do projeto', () => {
-    beforeEach( () =>{
-        pacote.calcularPacote.mockReturnValue('pacote_basico');
-    })
-    test('retornar valor total para um projeto dada as funcionalidades', () =>{
-       // A soma dá 72
-        const funcionalidades = [
-            'setup',
-            'responsividade',
-            'construcao_1_pagina',
-            'construcao_1_pagina',
-            'construcao_1_pagina',
-            'formulario',
-            'ssr'
-        ];
-        const valorHora = 70;
+  beforeEach(() => {
+    pacote.calcularPacote.mockReturnValue('pacote_basico');
+  });
 
-        const result = calcularValorTotalProjeto(funcionalidades, valorHora);
+  test('retornar valor total para um projeto dada as funcionalidades', () => {
+    const funcionalidades = [
+      'setup',
+      'responsividade',
+      'construcao_1_pagina',
+      'integracao_mailchimp',
+      'otimizacao_seo',
+      'integracao_api_propria',
+      'formulario',
+      'ssr'
+    ];
+    const valorHora = 70;
 
-        expect(result).toEqual(3696)
-    })
-})
+    const result = calcularValorTotalProjeto(funcionalidades, valorHora);
+
+    expect(result).toEqual(3696);
+  });
+
+  test('verificar se a função "calcularPacote" é chamada corretamente', () => {
+    const funcionalidades = [
+      'setup',
+      'responsividade',
+      'construcao_1_pagina',
+      'integracao_mailchimp',
+      'otimizacao_seo',
+      'integracao_api_propria',
+      'formulario',
+      'ssr'
+    ];
+    const valorHora = 70;
+
+    calcularValorTotalProjeto(funcionalidades, valorHora);
+
+    expect(pacote.calcularPacote).toHaveBeenCalledWith(funcionalidades);
+  });
+
+  test('verificar se o valor total do projeto é calculado corretamente', () => {
+    const funcionalidades = [
+      'setup',
+      'responsividade',
+      'construcao_1_pagina',
+      'integracao_mailchimp',
+      'otimizacao_seo',
+      'integracao_api_propria',
+      'formulario',
+      'ssr'
+    ];
+    const valorHora = 70;
+    const totalHorasFuncionalidades = funcionalidades.reduce(
+      (total, funcionalidade) => total + HORAS_POR_FUNCIONALIDADE[funcionalidade],
+      0
+    );
+    const totalHorasProjeto = totalHorasFuncionalidades * DIAS_UTEIS_NO_MES;
+    const pacoteSelecionado = 'pacote_basico';
+    const taxaContratual = TAXAS_CONTRATUAIS_POR_PACOTE[pacoteSelecionado];
+    const valorTotal = totalHorasProjeto * valorHora * taxaContratual;
+
+    const result = calcularValorTotalProjeto(funcionalidades, valorHora);
+
+    expect(result).toEqual(valorTotal);
+  });
+});
