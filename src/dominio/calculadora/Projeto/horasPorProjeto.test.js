1af80a27f9cdedfac6956ae583a9193ceff3fb39
Heloiza Mendes <helocursos@gmail.com>
2023-05-22 15:29:32 -0300

    Heloiza Mendes - entrega do desafio final

--- src/dominio/calculadora/Projeto/horasPorProjeto.test.js
+++ src/dominio/calculadora/Projeto/horasPorProjeto.test.js
@@ -3,14 +3,14 @@ const { calcularHorasDeProjeto } = require('./horasPorProjeto');
 describe('Cálculo de horas totais para finalizar um projeto', () => {
     test('Dado um array com as funcionalidades desejadas no projeto, retornar a soma das horas das funcionalidades', () => {
         const funcionalidades = [
-            'formulario', //16
-            'setup', //8
-            'responsividade', //16
-            'otimizacao_seo',//16
-            'construcao_1_pagina',//8
-            'integracao_mailchimp',//16
-            'ssr',//8
-            'integracao_api_propria',//16
+            'formulario',
+            'setup', 
+            'responsividade', 
+            'otimizacao_seo',
+            'construcao_1_pagina',
+            'integracao_mailchimp',
+            'ssr',
+            'integracao_api_propria',
         ]
 
 
