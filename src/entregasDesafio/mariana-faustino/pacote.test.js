4a0446bfb5d08061ed1d3878807910b83e487581
Mariana Faustino <marifaustino@gmail.com>
2023-05-22 16:31:29 -0300

    Condição pra projeto sem cobertura/pacote e testes

--- src/entregasDesafio/mariana-faustino/pacote.test.js
+++ src/entregasDesafio/mariana-faustino/pacote.test.js
@@ -7,4 +7,20 @@ describe('Calcular pacote correto para cada projeto', () =>{
 
         expect(result).toEqual('pacote_basico')
     })
+    test('Retorna pacote intermediário caso o número total seja de 51 até 100', () => {
+        const totalDeHorasPorProjeto = 99
+        const result = calcularPacote(totalDeHorasPorProjeto)
+
+        expect(result).toEqual('pacote_intermediario')
+    })
+    test('Retorna pacote premium caso o número total seja 101 até 200', () => {
+        const totalDeHorasPorProjeto = 150
+        const result = calcularPacote(totalDeHorasPorProjeto)
+
+        expect(result).toEqual('pacote_premium')
+    })
+    test('Lançar erro caso o total de horas ultrapasse 200', ()=>{
+        const totalDeHorasPorProjeto = 206
+        expect(()=> calcularPacote(totalDeHorasPorProjeto)).toThrow(Error)
+    })
 })
